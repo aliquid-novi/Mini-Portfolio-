6f92cf52368821d2421ece9e66486dd86145f0ba
Mark Paje <89901306+aliquid-novi@users.noreply.github.com>
2022-12-07 14:22:17 +1100

    Find function value based on 3 rules

---
+++ Precalc Models/Function Calculator.py
@@ -0,0 +1,10 @@
+import math
+def get_value(x):
+    if x <= -3:
+        x = x + 5
+        print(value)
+    elif x > -3 and x <+ 3:
+        value = math.sqrt(9 - x ** 2)
+    else:
+        x = -x + 5
+    return x
